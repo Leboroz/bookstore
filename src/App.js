76ebcd41ae1bbd616cbac28dc2e3f307eb521b54
Leonardo <albornozleo95@gmail.com>
2022-02-21 16:50:25 -0400

    Add base files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+import './index.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
