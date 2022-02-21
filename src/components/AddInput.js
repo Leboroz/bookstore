c7cac40bbdc42470f8d0ddb2787fd8d245e2e5d5
Leonardo <albornozleo95@gmail.com>
2022-02-21 17:37:46 -0400

    Fix linters

--- src/components/AddInput.js
+++ src/components/AddInput.js
@@ -2,11 +2,11 @@ const AddInput = () => (
   <div>
     <h2>ADD NEW BOOK</h2>
     <form action="">
-      <label>
-        <input type="text" placeholder="Book title" />
+      <label htmlFor="title">
+        <input id="title" type="text" placeholder="Book title" />
       </label>
-      <label>
-        <input type="text" placeholder="Category" />
+      <label htmlFor="category">
+        <input id="category" type="text" placeholder="Category" />
       </label>
       <button type="submit">ADD BOOK</button>
     </form>
