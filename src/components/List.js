458ca48b71c05dd4e25f380e00eaac05afafdc54
Leonardo <albornozleo95@gmail.com>
2022-02-21 16:51:32 -0400

    Create List Component

---
+++ src/components/List.js
@@ -0,0 +1,38 @@
+import Book from './Book';
+import styles from '../sass/components/list.module.scss';
+
+const List = () => {
+  const { list } = styles;
+  const books = [
+    {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      genre: 'action',
+      chapter: 'Chapter 17',
+    },
+    {
+      title: 'Dune',
+      author: 'Frank Herbert',
+      genre: 'Science Fiction',
+      chapter: 'Chapter 3',
+    },
+    {
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+      genre: 'Economy',
+      chapter: 'Introduction',
+    },
+  ];
+
+  return (
+    <ul className={list}>
+      {books.map((book) => (
+        <li key={book.title}>
+          <Book props={book}></Book>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
