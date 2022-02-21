aee7743422ba288b3eb56abcada61d934149e8ac
Leonardo <albornozleo95@gmail.com>
2022-02-21 16:50:44 -0400

    Add form

---
+++ src/components/AddInput.js
@@ -0,0 +1,18 @@
+const AddInput = () => {
+  return (
+    <div>
+      <h2>ADD NEW BOOK</h2>
+      <form action="">
+        <label>
+          <input type="text" placeholder="Book title" />
+        </label>
+        <label>
+          <input type="text" placeholder="Category" />
+        </label>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddInput;
