bfe6f177f9c0ebbcb2447f2e23689f28bb302311
Leonardo <albornozleo95@gmail.com>
2022-02-23 18:59:14 -0400

    Add redux-thunk to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './books/books';
 import thunk from 'redux-thunk';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
